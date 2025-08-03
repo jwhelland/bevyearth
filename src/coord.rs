29a037e43b025de671cd8784713ab33c0bfb05f5
Jason Helland <jwhelland@gmail.com>
2025-08-03 07:17:21 +0100

    initial commit

---
+++ src/coord.rs
@@ -0,0 +1,109 @@
+use bevy::prelude::Vec3;
+use std::f32::consts::PI;
+
+use crate::earth::EARTH_RADIUS_KM;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct CoordError {
+    pub msg: String,
+}
+
+#[derive(Debug)]
+pub struct Coordinates {
+    // Stored internally in radians (because math)
+    pub latitude: f32,
+    pub longitude: f32,
+}
+
+impl From<Vec3> for Coordinates {
+    fn from(value: Vec3) -> Self {
+        let normalized_point = value.normalize();
+        let latitude = normalized_point.y.asin();
+        let longitude = normalized_point.x.atan2(normalized_point.z);
+        Coordinates {
+            latitude,
+            longitude,
+        }
+    }
+}
+
+impl Coordinates {
+    pub fn as_degrees(&self) -> (f32, f32) {
+        let latitude = self.latitude * (180.0 / PI);
+        let longitude = self.longitude * (180.0 / PI);
+        (latitude, longitude)
+    }
+
+    pub fn convert_to_uv_mercator(&self) -> (f32, f32) {
+        let (lat, lon) = self.as_degrees();
+        let v = map_latitude(lat).unwrap();
+        let u = map_longitude(lon).unwrap();
+        (u, v)
+    }
+
+    #[allow(dead_code)]
+    pub fn from_degrees(latitude: f32, longitude: f32) -> Result<Self, CoordError> {
+        if !(-90.0..=90.0).contains(&latitude) {
+            return Err(CoordError {
+                msg: "Invalid latitude: {lat:?}".to_string(),
+            });
+        }
+        if !(-180.0..=180.0).contains(&longitude) {
+            return Err(CoordError {
+                msg: "Invalid longitude: {lon:?}".to_string(),
+            });
+        }
+        let latitude = latitude / (180.0 / PI);
+        let longitude = longitude / (180.0 / PI);
+        Ok(Coordinates {
+            latitude,
+            longitude,
+        })
+    }
+
+    pub fn get_point_on_sphere(&self) -> Vec3 {
+        let y = self.latitude.sin();
+        let r = self.latitude.cos();
+        let x = self.longitude.sin() * r;
+        let z = self.longitude.cos() * r;
+        Vec3::new(x, y, z).normalize() * EARTH_RADIUS_KM
+    }
+}
+
+// Maps a value from one range to another
+fn map((in_min, in_max): (f32, f32), (out_min, out_max): (f32, f32), value: f32) -> f32 {
+    (value - in_min) / (in_max - in_min) * (out_max - out_min) + out_min
+}
+
+fn map_latitude(lat: f32) -> Result<f32, CoordError> {
+    // 90 -> 0 maps to 0.0 to 0.5
+    // 0 -> -90 maps to 0.5 to 1.0
+    // Ensure latitude is valid
+    if !(-90.0..=90.0).contains(&lat) {
+        return Err(CoordError {
+            msg: "Invalid latitude: {lat:?}".to_string(),
+        });
+    }
+    if (90.0..=0.0).contains(&lat) {
+        Ok(map((90.0, 0.0), (0.0, 0.5), lat))
+    } else {
+        Ok(map((0.0, -90.0), (0.5, 1.0), lat))
+    }
+}
+
+fn map_longitude(lon: f32) -> Result<f32, CoordError> {
+    // -180 -> 0 maps to 0.0 to 0.5
+    // 0 -> 180 maps to 0.5 to 1.0
+    //Ensure longitude is valid
+    if !(-180.0..=180.0).contains(&lon) {
+        return Err(CoordError {
+            msg: "Invalid longitude: {lon:?}".to_string(),
+        });
+    }
+    if (-180.0..=0.0).contains(&lon) {
+        Ok(map((-180.0, 0.0), (0.0, 0.5), lon))
+    } else {
+        Ok(map((0.0, 180.0), (0.5, 1.0), lon))
+    }
+}
